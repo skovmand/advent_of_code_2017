a9ff384cbcdb6eb83fdc42e9e04d1f89a44ab93e
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2021-03-06 22:23:28 +0100

    D1P2 plus refactoring

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -3,20 +3,60 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("input/day01.txt").expect("Unable to read file");
 
-    let part_1_answer = inverse_capcha(input);
+    let part_1_answer = inverse_capcha(input.clone());
     println!("The answer to D1P1 is {}", part_1_answer);
+
+    let part_2_answer = inverse_circular_capcha(input);
+    println!("The answer to D1P2 is {}", part_2_answer);
 }
 
+/* Solver for D1P1 */
 pub fn inverse_capcha(input: String) -> u32 {
-    let number_vec: Vec<u32> = create_number_vec(&input);
-    let number_vec_offset_one = offset_number_vec(&number_vec);
+    let number_vec: Vec<u32> = parse_input(&input);
+    let number_vec_offset_one = offset_vec_by_one(&number_vec);
 
-    number_vec.iter().zip(number_vec_offset_one.iter())
-        .map(|(x,y)| if x == y { x } else { &0 })
+    number_vec
+        .iter()
+        .zip(number_vec_offset_one.iter())
+        .map(|(x, y)| if x == y { x } else { &0 })
         .sum::<u32>()
 }
 
-fn create_number_vec(string_input: &String) -> Vec<u32> {
+/* Solver for D1P2 */
+pub fn inverse_circular_capcha(input: String) -> u32 {
+    let number_vec: Vec<u32> = parse_input(&input);
+
+    number_vec
+        .iter()
+        .enumerate()
+        .map(|(index, elem)| if elem == opposite(&number_vec, index) { elem } else { &0 })
+        .sum::<u32>()
+}
+
+/* Get the opposite value in the circle given a vector index */
+fn opposite(number_vec: &Vec<u32>, index: usize) -> &u32 {
+    let number_vec_length = number_vec.len();
+    if number_vec_length % 2 != 0 {
+        panic!("The vector must have an even length");
+    };
+
+    let calculated_index = (index + (number_vec_length / 2)) % number_vec_length;
+    number_vec.get(calculated_index).unwrap()
+}
+
+/* Given a vector of u32s, offset it by one by moving the first element to the last position */
+fn offset_vec_by_one(original_vec: &Vec<u32>) -> Vec<u32> {
+    let mut offset_vec_by_one: Vec<u32> = original_vec.clone();
+
+    let first_element = offset_vec_by_one[0];
+    offset_vec_by_one.push(first_element);
+    offset_vec_by_one.remove(0);
+
+    offset_vec_by_one
+}
+
+/* Create a vector of u32s from a string input */
+fn parse_input(string_input: &String) -> Vec<u32> {
     string_input
         .split("")
         .filter(|c| c != &"")
@@ -24,25 +64,24 @@ fn create_number_vec(string_input: &String) -> Vec<u32> {
         .collect::<Vec<u32>>()
 }
 
-fn offset_number_vec(original_vec: &Vec<u32>) -> Vec<u32> {
-    let mut offset_number_vec: Vec<u32> = original_vec.clone();
-
-    let first_element = offset_number_vec[0];
-    offset_number_vec.push(first_element);
-    offset_number_vec.remove(0);
-
-    offset_number_vec
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn d1_unit_tests() {
+    fn p1_unit_tests() {
         assert_eq!(inverse_capcha(String::from("1122")), 3);
         assert_eq!(inverse_capcha(String::from("1111")), 4);
         assert_eq!(inverse_capcha(String::from("1234")), 0);
         assert_eq!(inverse_capcha(String::from("91212129")), 9);
+    }
+
+    #[test]
+    fn p2_unit_tests() {
+        assert_eq!(inverse_circular_capcha(String::from("1212")), 6);
+        assert_eq!(inverse_circular_capcha(String::from("1221")), 0);
+        assert_eq!(inverse_circular_capcha(String::from("123425")), 4);
+        assert_eq!(inverse_circular_capcha(String::from("123123")), 12);
+        assert_eq!(inverse_circular_capcha(String::from("12131415")), 4);
     }
 }
