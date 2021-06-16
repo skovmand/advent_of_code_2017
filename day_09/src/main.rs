ebf95b00a4971d593ebbf71eda359c70889a12e2
Niels Kristian Hansen Skovmand <niels@physicalcode.com>
2021-06-16 08:51:55 +0200

    D9P2

--- day_09/src/main.rs
+++ day_09/src/main.rs
@@ -2,38 +2,35 @@ const PUZZLE_INPUT: &str = include_str!("../../puzzle_inputs/day_09.txt");
 
 fn main() {
     let score = calculate_score(PUZZLE_INPUT);
-    println!("Day 9 part 1: Score is {}", score);
+    println!("Day 9 part 1: Score is {}", score.0);
+    println!("Day 9 part 2: Char count is {}", score.1);
 }
 
-fn calculate_score(input: &str) -> u32 {
+fn calculate_score(input: &str) -> (u32, u32) {
     let mut level = 0;
     let mut score = 0;
     let mut is_garbage: bool = false;
-    let mut stream = input.chars();
+    let mut garbage_chars: u32 = 0;
+    let mut stream = input.trim().chars();
 
     while let Some(c) = stream.next() {
         match c {
-            '<' => is_garbage = true,
+            '<' if !is_garbage => is_garbage = true,
             '>' => is_garbage = false,
-            '{' => {
-                if !is_garbage {
-                    level += 1;
-                    score += level;
-                }
+            '{' if !is_garbage => {
+                level += 1;
+                score += level;
             }
-            '}' => {
-                if !is_garbage {
-                    level -= 1;
-                }
-            }
+            '}' if !is_garbage => level -= 1,
             '!' => {
                 stream.next();
             }
+            _ if is_garbage => garbage_chars += 1,
             _ => {}
         }
     }
 
-    score
+    (score, garbage_chars)
 }
 
 #[cfg(test)]
@@ -42,13 +39,24 @@ mod tests {
 
     #[test]
     fn calculates_group_scores() {
-        assert_eq!(calculate_score("{}"), 1);
-        assert_eq!(calculate_score("{{{}}}"), 6);
-        assert_eq!(calculate_score("{{},{}}"), 5);
-        assert_eq!(calculate_score("{{{},{},{{}}}}"), 16);
-        assert_eq!(calculate_score("{<a>,<a>,<a>,<a>}"), 1);
-        assert_eq!(calculate_score("{{<ab>},{<ab>},{<ab>},{<ab>}}"), 9);
-        assert_eq!(calculate_score("{{<!!>},{<!!>},{<!!>},{<!!>}}"), 9);
-        assert_eq!(calculate_score("{{<a!>},{<a!>},{<a!>},{<ab>}}"), 3);
+        assert_eq!(calculate_score("{}").0, 1);
+        assert_eq!(calculate_score("{{{}}}").0, 6);
+        assert_eq!(calculate_score("{{},{}}").0, 5);
+        assert_eq!(calculate_score("{{{},{},{{}}}}").0, 16);
+        assert_eq!(calculate_score("{<a>,<a>,<a>,<a>}").0, 1);
+        assert_eq!(calculate_score("{{<ab>},{<ab>},{<ab>},{<ab>}}").0, 9);
+        assert_eq!(calculate_score("{{<!!>},{<!!>},{<!!>},{<!!>}}").0, 9);
+        assert_eq!(calculate_score("{{<a!>},{<a!>},{<a!>},{<ab>}}").0, 3);
+    }
+
+    #[test]
+    fn counts_characters() {
+        assert_eq!(calculate_score("<>").1, 0);
+        assert_eq!(calculate_score("<random characters>").1, 17);
+        assert_eq!(calculate_score("<<<<>").1, 3);
+        assert_eq!(calculate_score("<{!>}>").1, 2);
+        assert_eq!(calculate_score("<!!>").1, 0);
+        assert_eq!(calculate_score("<!!!>>").1, 0);
+        assert_eq!(calculate_score(r#"<{o"i!a,<{i<a>"#).1, 10);
     }
 }
